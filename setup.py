a574e64601a830bcbee5d2d01cb81f8ee1abb81b
yann.gaste <yann.gaste@irt-systemx.fr>
2025-03-06 15:08:47 +0100

    ci: test python lib publish pipeline

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
+import os
 
-setup()
+version = "0.0.0"
+try:
+    version = os.environ['python_lib_version']
+except KeyError:  # bamboo_buildNumber isn't defined, so we're not running in Bamboo
+    pass
+
+setup(
+    version = version,
+)
